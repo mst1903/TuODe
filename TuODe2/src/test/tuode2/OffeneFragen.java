92deb580ad6ad88741521b22590f8f47f0abd5fb
Matze <Matze@192.168.0.5>
2014-01-22 23:31:58 +0100

    Offene Fragen: Filter nach Kategorien hinzugefügt

--- TuODe2/src/test/tuode2/OffeneFragen.java
+++ TuODe2/src/test/tuode2/OffeneFragen.java
@@ -1,5 +1,8 @@
 package test.tuode2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,14 +11,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.Spinner;
 
 
 public class OffeneFragen extends Activity {
 	
-	private Cursor cursor;
+	Cursor cursor;
+	String kategorie;
+	List<String> fragen = new ArrayList<String>();
+	List<Integer> fragenId = new ArrayList<Integer>();
+	ListView lv;
+	ArrayAdapter<String> ad;
+	Spinner filter;
 	
 //befüllt die Listview (dafür muss die id der listview aber wieder in id="@ListView1" geändert werden) ist wohl nicht geeignet um daten aus einer datenbank einzutragen?! */
 @Override
@@ -23,67 +33,63 @@ public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_offene_fragen);
 		
-	//String[] listItems = {"Element 1", "Element 2", "Element 3", "Element 4", "Element 5", "Element 6", "Element 7", "Element 8"};
-	
 	FragenDatenbank db = new FragenDatenbank(this);
 	SQLiteDatabase con = db.getWritableDatabase();
 	
-	String columns[] = {FragenDatenbank.id,FragenDatenbank.titel};
+	String columns[] = {FragenDatenbank.id,FragenDatenbank.titel,FragenDatenbank.kategorie};
 	cursor = con.query(FragenDatenbank.fragenTabelle, columns, null, null, null, null, null);
-	String fragen[];
-	fragen = new String[cursor.getCount()];
-	int i = 0;
 	
-	while(cursor.moveToNext()){
-		fragen[i] = cursor.getString(cursor.getColumnIndex(FragenDatenbank.titel));
-		i++;
-	}
+	filter = (Spinner)findViewById(R.id.sp_filter);
+	filter.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+		@Override
+		public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			kategorie = String.valueOf(filter.getSelectedItem());
+			fragen.clear();
+			fragenId.clear();
+			for(int i=0;i<cursor.getCount();i++){
+				cursor.moveToPosition(i);
+				if (kategorie.equals("Alle Kategorien")) {
+						fragen.add(cursor.getString(cursor
+								.getColumnIndex(FragenDatenbank.titel)));
+						fragenId.add(cursor.getInt(cursor.getColumnIndex(FragenDatenbank.id)));
+				}
+				else{
+					if(kategorie.equals(cursor.getString(cursor.getColumnIndex(FragenDatenbank.kategorie)))){
+						fragen.add(cursor.getString(cursor
+								.getColumnIndex(FragenDatenbank.titel)));
+						fragenId.add(cursor.getInt(cursor.getColumnIndex(FragenDatenbank.id)));
+					}
+				}
+			}
+			ad.notifyDataSetChanged();
+		}
+		
+		public void onNothingSelected(AdapterView<?> arg0) {
+			
+		}
+	});
 	
-	ListView lv = (ListView) findViewById(R.id.list);
-	lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fragen));
-	
+	lv = (ListView) findViewById(R.id.list);
+	ad = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, fragen);
+	lv.setAdapter(ad);
 	lv.setClickable(true);
 	lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 		@Override
 		public void onItemClick(AdapterView<?> arg0, View arg1, int position,
 				long arg3) {
-			cursor.moveToPosition(position);
-			int id = cursor.getInt(cursor.getColumnIndex(FragenDatenbank.id));
-			frageAnzeigen(id);
-			
-			
+			frageAnzeigen(fragenId.get(position));
 		}
-
-		
 	});
-	
 }
 	
-/*	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_offene_fragen);
-		
-		ArrayList<String> valueList = new ArrayList<String>();
-		for (int i = 0; i < 10; i++)
-		{
-		valueList.add("value"+i);
-		}
-		ListAdapter adapter = new ArrayAdapter<Frage>(getApplicationContext(), android.R.layout.simple_list_item_1);
-		
-	    final ListView lv = (ListView)findViewById(R.id.listView1);
-
-	    lv.setAdapter(adapter);
-	}*/
-	
-
 	public void frageAnzeigen(int id) {
 		Intent i = new Intent(this, FrageAnzeigen.class);
 		i.putExtra(FragenDatenbank.id, id);
 		startActivity(i);
-		
-	
+		finish();
 	}
 
 	@Override
